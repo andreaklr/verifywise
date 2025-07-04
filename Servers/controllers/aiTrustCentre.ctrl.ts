96d017bbe6ec9b3761c255a09f777ddd4612e826
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2025-07-03 20:01:50 -0400

    add ai-trust-centre-overview controller

---
+++ Servers/controllers/aiTrustCentre.ctrl.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { STATUS_CODE } from "../utils/statusCode.utils";
+import { sequelize } from "../database/db";
+import { createAITrustCentreOverviewQuery } from "../utils/aiTrustCentre.utils";
+
+export async function createAITrustCentreOverview(
+  req: Request,
+  res: Response
+): Promise<any> {
+  const transaction = await sequelize.transaction();
+  try {
+    const overviewData = req.body;
+    const result = await createAITrustCentreOverviewQuery(overviewData, transaction);
+
+    if (result) {
+      await transaction.commit();
+      return res.status(201).json(
+        STATUS_CODE[201]({
+          message: "AI Trust Centre overview created successfully",
+        })
+      );
+    }
+
+    await transaction.rollback();
+    return res.status(503).json(
+      STATUS_CODE[503]({
+        message: "Failed to create AI Trust Centre overview",
+      })
+    );
+  } catch (error) {
+    await transaction.rollback();
+    return res.status(500).json(STATUS_CODE[500]((error as Error).message));
+  }
+}
