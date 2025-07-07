f2a58b67a5be4ff11d31d5942aa701195ac1ee59
srwa-rsp <srwa.rashidpoor.1993@gmail.com>
2025-07-07 17:17:13 -0400

    add getAlTrustCentreOverview route

--- Servers/routes/aiTrustCentre.route.ts
+++ Servers/routes/aiTrustCentre.route.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import {
     createAITrustCentreOverview,
+    getAITrustCentreOverview,
 } from "../controllers/aiTrustCentre.ctrl";
 import authenticateJWT from "../middleware/auth.middleware";
 
 const router = express.Router();
 
+router.get("/overview", getAITrustCentreOverview);
 router.post("/overview", authenticateJWT, createAITrustCentreOverview);
 
 export default router;
