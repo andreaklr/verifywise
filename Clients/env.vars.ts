b37873bdf52af7731d008aea26194c405ca1d7cb
andreaklr <andrea@klaralabs.com>
2025-07-13 22:43:31 +0200

    Update env.vars.ts

--- Clients/env.vars.ts
+++ Clients/env.vars.ts
@@ -1,11 +1,12 @@
 export const ENV_VARs = {
   URL:
-    import.meta.env.VITE_APP_API_BASE_URL ?? // keep empty string if set
-    (typeof window !== "undefined" // only run in browser
+    import.meta.env.VITE_APP_API_BASE_URL ??
+    (typeof window !== "undefined"
       ? `${window.location.protocol}//${window.location.hostname}${
           window.location.protocol === "https:" ? "" : ":3000"
-        }` // use current URL if not set
-      : "http://localhost:3000/"), // final Node/SSR fallback
+        }`
+      : "https://accordantai2back.onrender.com"), // fallback per SSR o ambienti server
+
   IS_DEMO_APP: import.meta.env.VITE_IS_DEMO_APP === "true",
   IS_MULTI_TENANT: import.meta.env.VITE_IS_MULTI_TENANT === "true",
 };
